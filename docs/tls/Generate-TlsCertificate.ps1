52491266333618024259f9094f00e464c9b426ab
Gabriel Weyer <gabrielweyer@gmail.com>
2019-08-26 21:43:00 +1000

    Use Docker instead of WSL to generate TLS cert

--- docs/tls/Generate-TlsCertificate.ps1
+++ docs/tls/Generate-TlsCertificate.ps1
@@ -1,3 +1,31 @@
+#requires -Version 6.0
+
+<#
+
+.SYNOPSIS
+Generate a TLS certificate for local development
+
+.DESCRIPTION
+Generate a Certificate Authority and a TLS certificate for localtest.me and *.localtest.me.
+
+.PARAMETER TrustCa
+Automatically trust the Certificate Authority.
+This switch is only available on Windows.
+
+.EXAMPLE
+./Generate-TlsCertificate.ps1
+
+.EXAMPLE
+./Generate-TlsCertificate.ps1 -TrustCa
+
+.NOTES
+Requires Docker to be installed.
+
+.LINK
+https://github.com/gabrielweyer/nuggets/
+
+#>
+
 [CmdletBinding()]
 Param (
     [switch]
@@ -6,124 +34,151 @@ Param (
 
 $ErrorActionPreference = 'Stop'
 
+if (!$IsWindows -and $TrustCa) {
+    throw 'The -TrustCa switch is only available on Windows for the moment'
+}
+
 try {
-    wsl /bin/bash --version 1>$null
+    docker --version 1>$null
 }
 catch {
-    Write-Error 'Windows Subsystem for Linux is not installed, please refer to the README'
+    throw 'Docker is not installed'
 }
 
-Write-Host ''
-Write-Host '- Creating the CA RSA key'
-Write-Host ''
+try {
+    Push-Location "$PSScriptRoot/out"
 
-# https://www.openssl.org/docs/manmaster/man1/genpkey.html
-wsl openssl genpkey `
-    -algorithm RSA `
-    -pkeyopt rsa_keygen_bits:2048 `
-    -out out/ca.key
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+    Write-Host -ForegroundColor Magenta '| Running Docker image to generate TLS certificate'
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
 
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Creating the CA RSA key returned exit code $LASTEXITCODE"
-}
+    Write-Verbose "Current directory is '$PWD'"
 
-Write-Host ''
-Write-Host '- Creating the CA certificate'
-Write-Host ''
+    docker run --detach --interactive --rm --tty `
+        --name cert-maker `
+        -v "${PWD}:/tls" `
+        -w '/tls' `
+        node | Out-Null
 
-# https://www.openssl.org/docs/manmaster/man1/req.html
-wsl openssl req `
-    -x509 `
-    -config ca.cnf `
-    -new `
-    -nodes `
-    -key out/ca.key `
-    -sha256 `
-    -days 365 `
-    -out out/ca.crt
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Creating the CA certificate returned exit code $LASTEXITCODE"
-}
-
-Write-Host ''
-Write-Host '- Creating the "localtest.me" TLS server certificate RSA key'
-Write-Host ''
-
-wsl openssl genpkey `
-    -algorithm RSA `
-    -pkeyopt rsa_keygen_bits:2048 `
-    -out out/localtest.me.key
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Creating the ""localtest.me"" TLS server certificate RSA key returned exit code $LASTEXITCODE"
-}
-
-Write-Host ''
-Write-Host '- Creating the CSR for "localtest.me"'
-Write-Host ''
-
-wsl openssl req `
-    -config localtest.me.cnf `
-    -new `
-    -nodes `
-    -key out/localtest.me.key `
-    -sha256 `
-    -reqexts v3_req `
-    -days 365 `
-    -out out/localtest.me.csr
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Creating the CSR for ""localtest.me"" returned exit code $LASTEXITCODE"
-}
-
-Write-Host ''
-Write-Host '- Creating the TLS server certificate for "localtest.me"'
-Write-Host ''
-
-wsl openssl x509 `
-    -req `
-    -in out/localtest.me.csr `
-    -CA out/ca.crt `
-    -CAkey out/ca.key `
-    -CAcreateserial `
-    -extfile localtest.me.cnf `
-    -extensions v3_req `
-    -sha256 `
-    -days 365 `
-    -out out/localtest.me.crt
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Creating the TLS server certificate for ""localtest.me"" returned exit code $LASTEXITCODE"
-}
-
-Write-Host ''
-Write-Host '- Trusting the Certificate Authority'
-Write-Host ''
-
-if ($TrustCa) {
-    Write-Host 'Converting the CA certificate to a PKCS#12'
-
-    wsl openssl pkcs12 `
-        -export `
-        -in out/ca.crt `
-        -inkey out/ca.key `
-        -out out/ca.p12 `
-        -passout pass:password `
-        -name "Local Development CA"
-    
     if ($LASTEXITCODE -ne 0) {
-        Write-Error "Converting the CA certificate to a PKCS#12 returned exit code $LASTEXITCODE"
+        Throw "Running the Node.js Docker image returned exit code $LASTEXITCODE"
     }
 
-    Write-Warning 'Windows will display a prompt demanding you if you want to install this Certificate Authority''s certificate'
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+    Write-Host -ForegroundColor Magenta '| Creating the Certificate Authority RSA key'
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
 
-    $Path = Join-Path $PSScriptRoot "out\ca.p12"
-    $CertificateAuthority = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($Path, 'password')
-    $Store = get-item cert:\CurrentUser\Root
-    $Store.Open("ReadWrite")
-    $Store.Add($CertificateAuthority)
-    $Store.Close()
-} else {
-    Write-Host 'The switch "TrustCa" was not used, we won''t install this Certificate Authority certificate'
+    # https://www.openssl.org/docs/manmaster/man1/genpkey.html
+    docker exec cert-maker openssl genpkey `
+        -algorithm RSA `
+        -pkeyopt rsa_keygen_bits:2048 `
+        -out ./ca.key
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "Creating the Certificate Authority RSA key returned exit code $LASTEXITCODE"
+    }
+
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+    Write-Host -ForegroundColor Magenta '| Creating the Certificate Authority certificate'
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+
+    # https://www.openssl.org/docs/manmaster/man1/req.html
+    docker exec cert-maker openssl req `
+        -x509 `
+        -config ./configuration/ca.cnf `
+        -new `
+        -nodes `
+        -key ./ca.key `
+        -sha256 `
+        -days 365 `
+        -out ./ca.crt
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "Creating the Certificate Authority certificate returned exit code $LASTEXITCODE"
+    }
+
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+    Write-Host -ForegroundColor Magenta '| Creating the "localtest.me" TLS server certificate RSA key'
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+
+    docker exec cert-maker openssl genpkey `
+        -algorithm RSA `
+        -pkeyopt rsa_keygen_bits:2048 `
+        -out ./tls-cert.key
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "Creating the 'localtest.me' TLS server certificate RSA key returned exit code $LASTEXITCODE"
+    }
+
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+    Write-Host -ForegroundColor Magenta '| Creating the Certificate Signing Request for "localtest.me"'
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+
+    docker exec cert-maker openssl req `
+        -config ./configuration/localtest.me.cnf `
+        -new `
+        -nodes `
+        -key ./tls-cert.key `
+        -sha256 `
+        -reqexts v3_req `
+        -out ./tls-cert.csr
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "Creating the Certificate Signing Request for 'localtest.me' returned exit code $LASTEXITCODE"
+    }
+
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+    Write-Host -ForegroundColor Magenta '| Creating the TLS server certificate for "localtest.me"'
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+
+    docker exec cert-maker openssl x509 `
+        -req `
+        -in ./tls-cert.csr `
+        -CA ./ca.crt `
+        -CAkey ./ca.key `
+        -CAcreateserial `
+        -extfile ./configuration/localtest.me.cnf `
+        -extensions v3_req `
+        -sha256 `
+        -days 365 `
+        -out ./tls-cert.crt
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "Creating the TLS server certificate for 'localtest.me' returned exit code $LASTEXITCODE"
+    }
+
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+    Write-Host -ForegroundColor Magenta '| Trusting the Certificate Authority'
+    Write-Host -ForegroundColor Magenta '-------------------------------------------------------------------'
+
+    if ($TrustCa) {
+        Write-Host -ForegroundColor Cyan 'Converting the Certificate Authority certificate to a PKCS#12'
+
+        docker exec cert-maker openssl pkcs12 `
+            -export `
+            -in ./ca.crt `
+            -inkey ./ca.key `
+            -out ./ca.p12 `
+            -passout pass:password `
+            -name 'Local Development CA'
+
+        if ($LASTEXITCODE -ne 0) {
+            Throw "Converting the Certificate Authority certificate to a PKCS#12 returned exit code $LASTEXITCODE"
+        }
+
+        Write-Warning "Windows will display a prompt demanding you if you want to install this Certificate Authority's certificate"
+
+        $Path = Join-Path $PSScriptRoot 'out\ca.p12'
+        $CertificateAuthority = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($Path, 'password')
+        $Store = get-item cert:\CurrentUser\Root
+        $Store.Open('ReadWrite')
+        $Store.Add($CertificateAuthority)
+        $Store.Close()
+    } else {
+        Write-Host -ForegroundColor Cyan "The switch -TrustCa was not used, we won't install this Certificate Authority certificate"
+    }
+}
+finally {
+    docker container stop cert-maker | Out-Null
+    Pop-Location
 }
--- docs/tls/README.md
+++ docs/tls/README.md
@@ -1,29 +1,30 @@
-# Generate a TLS server certificate on Windows
+# Generate a TLS server certificate on Windows and macOS
 
-:rotating_light: this is still a work a progress.
-
-`Generate-TlsCertificate.ps1` generates a `TLS` server certificate for `localtest.me` ([localhost testing done right][locatest-me]).
+`Generate-TlsCertificate.ps1` generates a `TLS` server certificate for `localtest.me` and `*.localtest.me` ([localhost testing done right][locatest-me]).
 
 :rotating_light: this is only intended to be used on **your** machine for development purposes. For everything else please use [Let's Encrypt][lets-encrypt] :bow:.
 
 ## Goals
 
 - No configuration
-- Generates a `Certificate Authority` so that it can be trusted in [Firefox][mozilla-ca-certiifcate-store]
+- Generates a `Certificate Authority` so that it can be trusted in [Firefox][mozilla-ca-certiifcate-store] and other systems as required
 
 ## Pre-requisites
 
-- [Windows Subsystem for Linux][install-wsl]
+- [Docker][download-docker]
+- `PowerShell Core`
+  - [instructions for Windows][install-ps-core-windows]
+  - [instructions for macOS][install-ps-core-macos]
 
 ## Usage
 
-```posh
+```powershell
 .\Generate-TlsCertificate.ps1
 ```
 
 You can optionally decide to trust the `Certificate Authority`:
 
-```posh
+```powershell
 .\Generate-TlsCertificate.ps1 -TrustCa
 ```
 
@@ -33,30 +34,38 @@ You can optionally decide to trust the `Certificate Authority`:
 security.enterprise_roots.enabled
 ```
 
-and set it to `true`.
+and set it to `true`. The `Certificate Authority` needs to be added to the `Trusted Root Certification Authorities` store of the **Local Computer**.
 
 ## Troubleshooting
 
+The easiest way to troubleshoot is to get yourself a terminal using the same image that was used to create the `TLS` server certificate:
+
+```powershell
+docker run -it --rm -v "${PWD}/out:/tls" -w '/tls' node bash
+```
+
 ### Inspecting the Certificate Authority certificate
 
-```posh
-wsl openssl x509 -noout -text -in out/ca.crt
+```bash
+openssl x509 -noout -text -in ./ca.crt
 ```
 
 ### Inspecting the localtest.me CSR
 
-```posh
-wsl openssl req -noout -text -in out/localtest.me.csr
+```bash
+openssl req -noout -text -in ./tls-cert.csr
 ```
 
 ### Inspecting the localtest.me TLS server certificate
 
-```posh
-wsl openssl x509 -noout -text -in out/localtest.me.crt
+```bash
+openssl x509 -noout -text -in ./tls-cert.crt
 ```
 
 [locatest-me]: https://github.com/localtest-dot-me/localtest-dot-me.github.com#readme
 [lets-encrypt]: https://letsencrypt.org/
 [mozilla-ca-certiifcate-store]: https://www.mozilla.org/en-US/about/governance/policies/security-group/certs/
-[install-wsl]: https://docs.microsoft.com/en-us/windows/wsl/install-win10
 [add-root-to-firefox]: https://wiki.mozilla.org/CA:AddRootToFirefox
+[download-docker]: https://www.docker.com/get-started
+[install-ps-core-windows]: https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-windows?view=powershell-6
+[install-ps-core-macos]: https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-macos?view=powershell-6
